9ba70f966e47f74d8f5e82bca3e62299024f792c
aditprq <143347564+aditprq@users.noreply.github.com>
2025-03-04 11:56:28 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,14 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage });
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(bodyParser.json());
+app.use(
+    cors({
+      origin: "*",
+      methods: "GET,POST,PUT,DELETE",
+      allowedHeaders: "Content-Type,Authorization",
+    })
+  );
+  app.use(bodyParser.json());
 
 
 app.get("/test-firebase", async (req, res) => {
